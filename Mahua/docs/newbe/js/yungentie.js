bb7fd41ebc75323ae544c29e143646e0177260db
Newbe <472158246@qq.com>
2017-05-24 23:31:26 +0800

    麻花文库初始化

---
+++ Mahua/docs/newbe/js/yungentie.js
@@ -0,0 +1,8 @@
+var cloudTieConfig = {
+  url: document.location.href,
+  sourceId: "",
+  productKey: "c3ae745cdfe7449ca9014458d483fa79",
+  target: "cloud-tie-wrapper"
+};
+var yunManualLoad = true;
+Tie.loader("aHR0cHM6Ly9hcGkuZ2VudGllLjE2My5jb20vcGMvbGl2ZXNjcmlwdC5odG1s", true);
