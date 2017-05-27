9c8a4fd70d977a57b35f2e7d38d38a8ff08cb2c7
俞坤 <472158246@qq.com>
2017-05-27 13:02:53 +0800

    修改了菜单栏所属的模板

--- dockerStart.cmd
+++ dockerStart.cmd
@@ -1,2 +1,3 @@
 docker rm newbeJekyll -f
-docker run -v %~dp0:/usr/src/app -p "4000:4000" --name newbeJekyll starefossen/github-pages 
+start http://127.0.0.1:4000
+docker run -v %~dp0:/usr/src/app -p "4000:4000" --name newbeJekyll starefossen/github-pages
