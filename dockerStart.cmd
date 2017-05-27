ccbbcfb51ede808fb6cc11af63721e9b608b2c31
Newbe <472158246@qq.com>
2017-05-27 20:10:02 +0800

    延迟三秒打开浏览器

--- dockerStart.cmd
+++ dockerStart.cmd
@@ -1,3 +1,4 @@
 docker rm newbeJekyll -f
+docker run -v %~dp0:/usr/src/app -p "4000:4000" -d --name newbeJekyll starefossen/github-pages
+ping 127.0.0.1 -n 3 -w 1000 > nul
 start http://127.0.0.1:4000
-docker run -v %~dp0:/usr/src/app -p "4000:4000" --name newbeJekyll starefossen/github-pages
