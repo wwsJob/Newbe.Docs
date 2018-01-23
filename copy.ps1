890096fefc1100c5e7a7d73947df5bb8ae331fb1
Newbe <472158246@qq.com>
2018-01-23 22:05:20 +0800

    print

--- copy.ps1
+++ copy.ps1
@@ -7,7 +7,7 @@
 Get-ChildItem .\docs\* -File -Recurse | Where-Object { $_.FullName.EndsWith(".html") -or $_.FullName.EndsWith(".xml")} | ForEach-Object {
     $content = Get-Content $_.FullName -Encoding "utf8" | Out-String
     if ($content.Contains("http://0.0.0.0:4000")) {
-        $content = $content.Replace("http://0.0.0.0:4000","http://www.newbe.pro")
+        $content = $content.Replace("http://0.0.0.0:4000","http://www.newbe.cf")
         $content | Out-File -FilePath $_.FullName -Encoding "utf8"
     }
 }
